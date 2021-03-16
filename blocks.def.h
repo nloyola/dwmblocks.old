8c14b1829f9705d068d8bb16fdc8f5bdbf865b5d
Nelson Loyola <nloyola@gmail.com>
2021-03-16 10:54:13 +0100

    Added volume

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+/* sb-volume taken from https://github.com/LukeSmithxyz/dwmblocks */
+
+static const Block blocks[] = {
+    /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    {"", "sb-volume",                                                   0,             10},
+    {"", "date '+%a %b %d %I:%M%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
